1d07450fdde98bee7e2be5c95d30f896a6dcc6f5
VonIsaac <119748258+VonIsaac@users.noreply.github.com>
2025-09-01 21:05:42 +0800

    fixing login

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,55 +1,48 @@
-import { View, Text, StyleSheet,Button, ImageBackground } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
- import { Link } from 'expo-router' 
+import { ImageBackground } from 'expo-image'
+import { Link } from 'expo-router'
 
 const App = () => {
-
-  const handleFunction = () => {
-    console.log("Function called");
-  }
   return (
-    <View style={styles.container}>
-      <ImageBackground  source={require('@/assets/images/tsokolatea.jpg')} style={styles.image}>
-        <Text style={styles.text}>Coffe Shop</Text>
-          <Link href="/explore" asChild>
-            <Text style={styles.linkText}>Go to Explore</Text>
-          </Link>
-          <Button title="Call Function" onPress={handleFunction} />
-      </ImageBackground>
-      
+    <View style ={styles.container}>
+        <ImageBackground source={require('@/assets/images/mtea.jpg')} style={styles.image}>
+            <Text style={styles.title}>Tsokolatea</Text>
+            <Link href="/pages/user/login">
+                <Text style={styles.linkText}>Browse</Text>
+            </Link>
+        </ImageBackground>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100%',
 
-  },
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    resizeMode: 'cover',
-    backgroundRepeat: 'no-repeat',
-    width: '100%',
-    height: '100%',
-  },
-  text: {
-    color: 'red',
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  linkText: {
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+        marginTop: 20,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+    },
+      linkText: {
     color: 'blue',
     fontSize: 18,
     textDecorationLine: 'underline',
     marginTop: 20,
     marginBottom: 20,
+    textAlign: 'center',
   },
 })
